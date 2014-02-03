f5a85c341aeb22aef90ed3c85da45dcbff382b1a
Bernd Lietzow <github@ebrnd.de>
2014-02-03 20:01:09 +0100

    list of screen settings

--- randrpresets.py
+++ randrpresets.py
@@ -3,8 +3,10 @@ from gi.repository import Gtk
 
 class RandrPresetsWindow(Gtk.Window):
 
-  def __init__(self):
+  def __init__(self, presets):
     Gtk.Window.__init__(self, title="RandRpresets")
+
+    self.presets = presets
 
     self.set_border_width(10)
     self.set_default_size(200, 200)
@@ -16,41 +18,30 @@ class RandrPresetsWindow(Gtk.Window):
     listbox.set_selection_mode(Gtk.SelectionMode.NONE)
     hbox.pack_start(listbox, True, True, 0)
 
-    # XXX this will be done in a loop later
-    row = Gtk.ListBoxRow()
-    hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=30)
-    row.add(hbox)
+    for preset in self.presets:
+      row = Gtk.ListBoxRow()
+      hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=30)
+      row.add(hbox)
 
-    # XXX this will be loaded from the "preset" object
-    activate_button = Gtk.Button(label="Preset 1")
-    hbox.pack_start(activate_button, True, True, 0)
-    display_a_check_button = Gtk.CheckButton(label="eDP1")
-    hbox.pack_start(display_a_check_button, True, True, 0)
-    display_b_check_button = Gtk.CheckButton(label="VGA1")
-    hbox.pack_start(display_b_check_button, True, True, 0)
-    display_c_check_button = Gtk.CheckButton(label="HDMI1")
-    hbox.pack_start(display_c_check_button, True, True, 0)
+      activate_button = Gtk.Button(label=preset.name)
+      hbox.pack_start(activate_button, True, True, 0)
+      for screen in preset.screens:
+        screen_check_button = Gtk.CheckButton(label=screen[0])
+        screen_check_button.set_active(screen[1])
+        hbox.pack_start(screen_check_button, True, True, 0)
 
-    listbox.add(row)
+      listbox.add(row)
 
-    # XXX another iteration...
-    row = Gtk.ListBoxRow()
-    hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=30)
-    row.add(hbox)
+class Preset:
+  def __init__(self, name, screens=[]):
+    self.name = name
+    self.screens = screens
 
-    # XXX this will be loaded from the "preset" object
-    activate_button = Gtk.Button(label="Preset 2")
-    hbox.pack_start(activate_button, True, True, 0)
-    display_a_check_button = Gtk.CheckButton(label="eDP1")
-    hbox.pack_start(display_a_check_button, True, True, 0)
-    display_b_check_button = Gtk.CheckButton(label="VGA1")
-    hbox.pack_start(display_b_check_button, True, True, 0)
-    display_c_check_button = Gtk.CheckButton(label="HDMI1")
-    hbox.pack_start(display_c_check_button, True, True, 0)
-
-    listbox.add(row)
-
-win = RandrPresetsWindow()
+preset_a = Preset("Only internal", [["eDP1", True], ["VGA1", False], ["HDMI1", False]])
+preset_b = Preset("Internal+VGA", [["eDP1", True], ["VGA1", True], ["HDMI1", False]])
+preset_c = Preset("Internal+HDMI", [["eDP1", True], ["VGA1", False], ["HDMI1", True]])
+presets = [preset_a, preset_b, preset_c]
+win = RandrPresetsWindow(presets)
 win.connect("delete-event", Gtk.main_quit)
 win.show_all()
 Gtk.main()
